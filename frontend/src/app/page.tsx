b93cb85c45e55e4b21967837dd853cd51540e15c
t4ichi <taichinoheya020316@icloud.com>
2024-10-14 15:29:05 +0900

    feat: todo component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,24 +1,19 @@
 "use client";
 
-import { Button } from "@/components/base/atoms/Button";
 import { Title } from "@/components/base/atoms/Title";
+import { Todos } from "@/components/domain/todo/molecules/Todos";
+import { Todo } from "@/types/api";
 import { useState, useEffect } from "react";
 import React from "react";
 import { css } from "styled-system/css";
 
-type Todo = {
-  id: number;
-  title: string;
-  completed: boolean;
-};
-
-export default function Home() {
+const Home = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   useEffect(() => {
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/todos`)
       .then((res) => res.json())
-      .then(setTodos)
+      .then((data) => setTodos(data))
       .catch((err) => console.error("Failed to fetch todos:", err));
   }, []);
 
@@ -31,22 +26,11 @@ export default function Home() {
       })}
     >
       <div className={css({ textAlign: "center" })}>
-        <Title>Todo</Title>
-        <ul>
-          {todos.map((todo) => (
-            <div
-              key={todo.id}
-              className={css({
-                display: "flex",
-              })}
-            >
-              <input type="checkbox" name="" id="" />
-              <li>{todo.title}</li>
-              <Button>+</Button>
-            </div>
-          ))}
-        </ul>
+        <Title>タイトル</Title>
+        <Todos todos={todos} />
       </div>
     </div>
   );
-}
+};
+
+export default Home;
