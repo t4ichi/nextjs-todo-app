16b4d173d0989a8008fcfe893a2191f6284a5470
t4ichi <taichinoheya020316@icloud.com>
2024-10-27 17:40:44 +0900

    refactor: Update theme colors in panda.config.ts

--- frontend/panda.config.ts
+++ frontend/panda.config.ts
@@ -1,5 +1,37 @@
 import { defineConfig } from "@pandacss/dev";
 
+const theme = {
+  tokens: {
+    fonts: {
+      body: {
+        value: {},
+      },
+    },
+    colors: {
+      base: {
+        50: { value: "#F2F2F2" },
+        100: { value: "#E6E6E6" },
+        200: { value: "#CCCCCC" },
+        400: { value: "#999999" },
+        600: { value: "#666666" },
+        900: { value: "#1A1A1A" },
+      },
+      primary: {
+        50: { value: "#F0F9FF" },
+        400: { value: "#57B8FF" },
+        800: { value: "#0066BE" },
+      },
+      succcess: {
+        1: { value: "#259D63" },
+        2: { value: "#197A4B" },
+      },
+      error: {
+        1: { value: "#EC0000" },
+        2: { value: "#CE0000" },
+      },
+    },
+  },
+};
 export default defineConfig({
   // Whether to use css reset
   preflight: true,
@@ -11,6 +43,7 @@ export default defineConfig({
     "./src/app/**/*.{ts,tsx,js,jsx}",
   ],
 
+  theme: theme,
   // Files to exclude
   exclude: [],
 
