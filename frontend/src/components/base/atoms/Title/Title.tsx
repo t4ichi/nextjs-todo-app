6d27ea3bd3feee840fa5b0e25e12eaea9c676fdc
t4ichi <taichinoheya020316@icloud.com>
2024-10-28 00:31:22 +0900

    refactor: Update Title component to include forwardRef and additional props

--- frontend/src/components/base/atoms/Title/Title.tsx
+++ frontend/src/components/base/atoms/Title/Title.tsx
@@ -1,17 +1,15 @@
+import { ComponentProps, forwardRef } from "react";
 import { css } from "styled-system/css";
 
-type titleProps = {
-  children: React.ReactNode;
-};
-export const Title = ({ children }: titleProps) => {
-  return (
-    <h1
-      className={css({
-        color: "base.900",
-        fontWeight: "bold",
-      })}
-    >
-      {children}
-    </h1>
-  );
-};
+const titleStyle = css({
+  color: "base.900",
+  fontWeight: "bold",
+});
+
+export const Title = forwardRef<HTMLHeadingElement, ComponentProps<"h1">>(
+  (props, ref) => {
+    const { className, ...rest } = props;
+    return <h1 className={`${titleStyle} ${className}`} ref={ref} {...rest} />;
+  },
+);
+Title.displayName = "Title";
