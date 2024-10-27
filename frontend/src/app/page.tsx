d30183ff70aca64ff51d5dffe674f4aae420518c
t4ichi <taichinoheya020316@icloud.com>
2024-10-28 00:27:15 +0900

    refactor: Update page layout with todo logo and centered todos

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,18 +1,28 @@
-import { Title } from "@/components/base/atoms/Title";
 import { Todos } from "@/components/domain/todo/molecules/Todos";
 import { css } from "styled-system/css";
 import { getTodos } from "./api";
+import todoLogo from "@/public/images/logo/todo.svg";
+import Image from "next/image";
+
+const logoStyle = css({
+  display: "flex",
+  justifyContent: "center",
+  marginTop: "4rem",
+});
+
+const todosStyle = css({
+  display: "flex",
+  justifyContent: "center",
+});
 
 const Home = async () => {
   const todos = await getTodos();
   return (
-    <div
-      className={css({
-        alignItems: "center",
-      })}
-    >
-      <div className={css({ textAlign: "center" })}>
-        <Title>タイトル</Title>
+    <div>
+      <div className={logoStyle}>
+        <Image src={todoLogo} alt="logo" />
+      </div>
+      <div className={todosStyle}>
         <Todos todos={todos} />
       </div>
     </div>
