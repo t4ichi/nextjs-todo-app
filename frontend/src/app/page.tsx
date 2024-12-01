e1279e01d554c5549ce3a5430ff7dc64a44a6067
t4ichi <taichinoheya020316@icloud.com>
2024-12-01 17:29:02 +0900

    refactor: Remove unused div from Home component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,60 +8,55 @@ import { Button } from "@/components/base/atoms/Button";
 import { Divider } from "@/components/base/atoms/Divider";
 
 const logoStyle = css({
-  display: "flex",
-  justifyContent: "center",
-  marginTop: "1rem",
+	display: "flex",
+	justifyContent: "center",
+	marginTop: "1rem",
 });
 
 const todosStyle = css({
-  display: "flex",
-  justifyContent: "center",
+	display: "flex",
+	justifyContent: "center",
 });
 
 const Home = async () => {
-  const todos = await getTodos();
+	const todos = await getTodos();
 
-  return (
-    <div>
-      <div
-        className={css({
-          width: "100%",
-        })}
-      ></div>
-      <div className={logoStyle}>
-        <Image src={todoLogo} alt="logo" />
-      </div>
-      <div
-        className={css({
-          textAlign: "center",
-        })}
-      >
-        <div className={todosStyle}>
-          <div
-            className={css({
-              display: "flex",
-              gap: "0.5rem",
-              marginTop: "1em",
-            })}
-          >
-            <TextInput placeholder="タスクを入力" />
-            <Button>追加</Button>
-          </div>
-        </div>
+	return (
+		<div>
+			<div className={logoStyle}>
+				<Image src={todoLogo} alt="logo" />
+			</div>
+			<div
+				className={css({
+					textAlign: "center",
+				})}
+			>
+				<div className={todosStyle}>
+					<div
+						className={css({
+							display: "flex",
+							gap: "0.5rem",
+							marginTop: "1em",
+						})}
+					>
+						<TextInput placeholder="タスクを入力" />
+						<Button>追加</Button>
+					</div>
+				</div>
 
-        <div
-          className={css({
-            width: "80%",
-            marginTop: "2rem",
-            margin: "0 auto",
-          })}
-        >
-          <Divider />
-        </div>
-        <Todos todos={todos} />
-      </div>
-    </div>
-  );
+				<div
+					className={css({
+						width: "80%",
+						marginTop: "2rem",
+						margin: "0 auto",
+					})}
+				>
+					<Divider />
+				</div>
+				<Todos todos={todos} />
+			</div>
+		</div>
+	);
 };
 
 export default Home;
