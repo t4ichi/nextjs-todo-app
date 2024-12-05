bf27ab798d03a8d642b6bd982ebd5d98ee3b7623
t4ichi <taichinoheya020316@icloud.com>
2024-12-06 08:39:10 +0900

    refactor: simplify TodoCreateForm submission logic by removing redundant onSubmit function

--- frontend/src/components/domain/Todo/molecules/TodoCreateForm/TodoCreateFormContainer.tsx
+++ frontend/src/components/domain/Todo/molecules/TodoCreateForm/TodoCreateFormContainer.tsx
@@ -2,14 +2,10 @@ import { createTodo, type CreateTodoBody } from "@/app/api/fetchTodos";
 import { TodoCreateFormPresentational } from "./TodoCreateFormPresentational";
 
 export const TodoCreateForm = async () => {
-	const onSubmit = async (formData: CreateTodoBody) => {
-		"use server";
-		await createTodo(formData);
-	};
-	const handleSubmit = async (formData: FormData) => {
-		"use server";
-		const title = formData.get("title") as string;
-		await onSubmit({ title });
-	};
-	return <TodoCreateFormPresentational onSubmit={handleSubmit} />;
+  const handleSubmit = async (formData: FormData) => {
+    "use server";
+    const title = formData.get("title") as string;
+    await createTodo({ title });
+  };
+  return <TodoCreateFormPresentational onSubmit={handleSubmit} />;
 };
