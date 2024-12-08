1bf3f8dcb8f69069829bef4ee7b5f1405b2a348e
t4ichi <taichinoheya020316@icloud.com>
2024-12-08 14:42:16 +0900

    feat: enhance CheckBox component with improved styling and props forwarding

--- frontend/src/components/base/atoms/CheckBox/CheckBox.tsx
+++ frontend/src/components/base/atoms/CheckBox/CheckBox.tsx
@@ -1,27 +1,50 @@
-import { Checkbox } from "@ark-ui/react";
+import { Checkbox, type CheckboxRootProps } from "@ark-ui/react";
 import { CheckIcon } from "lucide-react";
 import { css } from "styled-system/css";
+
+export type CheckBoxProps = CheckboxRootProps;
 
 const checkboxRoot = css({
   display: "flex",
   alignItems: "center",
   cursor: "pointer",
+  _hover: {
+    opacity: "0.8",
+  },
 });
 
 const checkboxControl = css({
-  width: "1.5rem",
-  height: "1.5rem",
-  borderWidth: "1px",
+  width: "2.5rem",
+  height: "2.5rem",
+  borderWidth: "4px",
   borderColor: "primary.600",
+  borderRadius: "50%",
   borderStyle: "solid",
+  _checked: {
+    backgroundColor: "primary.600",
+  },
 });
 
-export const CheckBox = () => {
+const checkboxIndicator = css({
+  display: "flex",
+  width: "100%",
+  height: "100%",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+const checkboxIconStyle = css({
+  color: "primary.600",
+  '[data-state="checked"] &': {
+    color: "white",
+  },
+});
+export const CheckBox = ({ ...props }: CheckBoxProps) => {
   return (
-    <Checkbox.Root className={checkboxRoot}>
+    <Checkbox.Root className={checkboxRoot} {...props}>
       <Checkbox.Control className={checkboxControl}>
-        <Checkbox.Indicator>
-          <CheckIcon />
+        <Checkbox.Indicator className={checkboxIndicator}>
+          <CheckIcon className={checkboxIconStyle} />
         </Checkbox.Indicator>
       </Checkbox.Control>
       <Checkbox.HiddenInput />
