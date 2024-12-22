5a1794072169f9de91fae2400ce59b8e493be582
t4ichi <taichinoheya020316@icloud.com>
2024-12-22 17:05:27 +0900

    feat: update orval configuration to include input settings and enhance output structure for Todo and TodoZod

--- orval.config.ts
+++ orval.config.ts
@@ -1,15 +1,31 @@
 module.exports = {
   todo: {
+    input: {
+      target: "docs/open_api/todo.yml",
+    },
     output: {
-      target: "frontend/src/app/api/fetchTodos.ts",
-      client : "react-query",
+      // target: "frontend/src/gen/endpoints/todo/todo.ts",
+      target: "frontend/src/gen/endpoints/todo.ts",
+      mode: "tags-split",
+      client: "react-query",
+      schemas: "frontend/src/gen/models",
       override: {
         mutator: {
           path: "frontend/src/lib/customFetch.ts",
-          name: "customFetch"
-        }
-      }
+          name: "customFetch",
+        },
+      },
     },
-    input: "docs/open_api/todo.yml"
-  }
-}
+  },
+  todoZod: {
+    input: {
+      target: "docs/open_api/todo.yml",
+    },
+    output: {
+      mode: "tags-split",
+      client: "zod",
+      target: "frontend/src/gen/endpoints/todo.zod.ts",
+      fileExtension: ".zod.ts",
+    },
+  },
+};
